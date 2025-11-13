4bd8e6c6ec6da4ac58da6665a4769130c36af405
n0xgg04 <dulieu.vblc@gmail.com>
2025-11-13 19:23:31 +0700

    chore: update documentation structure and links, add overview page for Nezon

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://nezon.dev",
+  url: "https://nezon.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -82,7 +82,7 @@ const config: Config = {
           items: [
             {
               label: "Giới thiệu",
-              to: "/docs/",
+              to: "/docs/overview",
             },
           ],
         },
